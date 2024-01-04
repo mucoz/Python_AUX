6c576d021b7372ff1f9bd468edefbe276d1ab2d5
mucoz <72700035+mucoz@users.noreply.github.com>
2024-01-04 15:51:14 +0100

    Create desktop_webview.py

---
+++ desktop_webview.py
@@ -0,0 +1,16 @@
+import webview
+import threading
+import sys
+import app from app # app in app.py module
+
+
+if __name__ == '__main__':
+  # run flask app in a different thread
+  flask_app = threading.Thread(target=app.run)
+  flask_app.start()
+  # run GUI
+  webview.create_window(title='Application', url='http://127.0.0.1', width=800, height=600)
+  webview.start(http_server=True)
+  # after GUI, terminate flask app
+  sys.exit()
+  
