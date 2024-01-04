26015e308487947cc2d12c01ac1f474f9d1f0999
mucoz <72700035+mucoz@users.noreply.github.com>
2024-01-04 15:54:38 +0100

    Update desktop_webview.py

--- desktop_webview.py
+++ desktop_webview.py
@@ -5,12 +5,12 @@ from app import app # app in app.py module
 
 
 if __name__ == '__main__':
-  # run flask app in a different thread
-  flask_app = threading.Thread(target=app.run)
-  flask_app.start()
-  # run GUI
-  webview.create_window(title='Application', url='http://127.0.0.1', width=800, height=600)
-  webview.start(http_server=True)
-  # after GUI, terminate flask app
-  sys.exit()
+    # run flask app in a different thread
+    flask_app = threading.Thread(target=app.run)
+    flask_app.start()
+    # run GUI
+    webview.create_window(title='Application', url='http://127.0.0.1:5000', width=800, height=600)
+    webview.start(http_server=True)
+    # after GUI, terminate flask app
+    sys.exit()
   
